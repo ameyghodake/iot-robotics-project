ffda528f8fd04b5980767d0181ca23ade7b55ab5
ghodam3 <amey.ghodake@external.hella.com>
2024-12-04 12:38:35 +0530

    FIX: added corrections

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost/sensor_data"  # Update with your credentials
+DATABASE_URL = "postgresql://postgres-user:postgres@localhost/sensor_data"  # Update with your credentials
 
 # SQLAlchemy engine and session
 engine = create_engine(DATABASE_URL)
@@ -10,5 +10,5 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def init_db():
     from .models import Base
-
+    print("Initiating db connection")
     Base.metadata.create_all(bind=engine)
